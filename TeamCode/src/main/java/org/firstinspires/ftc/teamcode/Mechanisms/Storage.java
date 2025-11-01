e5f58df430e108aeafe82ca04db8a1e331fea169
Asimovldn <asimovldn@gmail.com>
2025-11-01 12:25:22 -0300

    Programação motor armazem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Storage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Storage.java
@@ -1,46 +1,87 @@
 package org.firstinspires.ftc.teamcode.Mechanisms;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.Control.FeedForwardCoefficients;
+import org.firstinspires.ftc.teamcode.Control.FeedForwardControl;
+import org.firstinspires.ftc.teamcode.Control.PIDCoefficients;
+import org.firstinspires.ftc.teamcode.Control.PIDControl;
+
+@Config
 public class Storage
 {
     DcMotorEx storageMotor;
     Servo liftServo;
 
     private final int TICK_PER_REV = 288;
-    private final int TICK_PER_DEGREE = TICK_PER_REV / 360;
+    private final double TICK_PER_DEGREE = (int)Math.round(TICK_PER_REV / 360.0);
 
     private boolean isAtShoot = false;
 
-    private double servoLiftPosition = 0.75;
-    private double servoDownPosition = 1.0;
+    private final double servoLiftPosition = 0.5;
+    private final double servoDownPosition = 0.91;
 
+    public static PIDCoefficients pidCoefficients = new PIDCoefficients(0,0,0);
+    public static FeedForwardCoefficients ffCoefficients = new FeedForwardCoefficients(0.001,0,0);
+
+    private double targetPosition = 0;
     public void init(HardwareMap hardwareMap)
     {
         storageMotor = hardwareMap.get(DcMotorEx.class, "storage_motor");
         storageMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
+        storageMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
         liftServo = hardwareMap.get(Servo.class, "lift_servo");
 
-        liftServo.setPosition(0.91);
+        liftServo.setPosition(servoDownPosition);
 
     }
 
-    public void move60()
+    public void setTargetDegrees(double degrees)
     {
-        storageMotor.setTargetPosition(storageMotor.getCurrentPosition() + TICK_PER_DEGREE * 60);
-        storageMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        storageMotor.setPower(0.1);
+        targetPosition = degrees;
     }
+
+    public void updateStorage()
+    {
+
+        if (Math.abs(getTargetPosition() - getMotorPosition()) > 5)
+        {
+            FeedForwardControl ffControl = new FeedForwardControl(ffCoefficients);
+            storageMotor.setPower(ffControl.calculate(90, 0, (int) Math.signum(getTargetPosition() - getMotorPosition())));
+        } else {
+            PIDControl pidControl = new PIDControl(pidCoefficients);
+            storageMotor.setPower(pidControl.calculate(getMotorPosition(), getTargetPosition()));
+        }
+
+    }
+
 
     public boolean isBusy()
     {
         return storageMotor.isBusy();
     }
 
+    public double getTargetPosition()
+    {
+        return targetPosition * TICK_PER_DEGREE;
+    }
+
+    public double getMotorVelocity()
+    {
+        return storageMotor.getVelocity(AngleUnit.DEGREES);
+    }
+
+    public int getMotorPosition()
+    {
+        return  storageMotor.getCurrentPosition();
+    }
 
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StorageTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StorageTest.java
@@ -1,13 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.Mechanisms.Storage;
 
 @TeleOp
@@ -32,18 +31,20 @@ public class StorageTest extends LinearOpMode
 
         int dir = 1;
 
+        storage.setTargetDegrees(60);
+
+        telemetry = new MultipleTelemetry(FtcDashboard.getInstance().getTelemetry(), telemetry);
+
         while (opModeIsActive())
         {
-            storageMotor.setVelocity(270 * dir, AngleUnit.DEGREES);
+           storage.updateStorage();
+           telemetry.addData("motorPos", storage.getMotorPosition());
+           telemetry.addData("targetPos", storage.getTargetPosition());
+           telemetry.addData("targetVel", 90);
+           telemetry.addData("motorVel", storage.getMotorVelocity());
+           telemetry.addData("error", storage.getTargetPosition() - storage.getMotorPosition());
+           telemetry.update();
 
-            if (storageMotor.getCurrentPosition() == lastPosition + dir * 120 * 288 / 360)
-            {
-                storageMotor.setVelocity(0);
-                lastPosition = storageMotor.getCurrentPosition();
-                dir = -dir;
-
-                sleep(3000);
-            }
         }
     }
 }
